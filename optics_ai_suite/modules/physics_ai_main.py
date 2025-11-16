1f053108c04f7255fb282409d93c8e0ae37c04c8
Ved Thakur <vedthakursa@gmail.com>
2025-11-16 15:35:14 +0530

    update

--- optics_ai_suite/modules/physics_ai_main.py
+++ optics_ai_suite/modules/physics_ai_main.py
@@ -14,7 +14,6 @@ def run():
         "quantum_tunneling_simulator": "Quantum Tunneling Simulator",
         "optical_fiber_visualizer": "Optical Fiber Visualizer",
         "photoelectric_effect_simulator": "Photoelectric Effect Simulator",
-        "hologram_visualizer": "Hologram Visualizer",
         "laser_divergence_simulator": "Laser Divergence Simulator",
         "compton_effect_explorer": "Compton Effect Explorer",
         "atomic_spectra_visualizer": "Atomic Spectra Visualizer",
